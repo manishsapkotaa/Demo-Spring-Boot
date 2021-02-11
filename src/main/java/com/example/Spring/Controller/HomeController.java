cedf139373830142ae59a1bc78caa77e88a716a0
manishsapkotaa <001manishsapkota@gmail.com>
2021-02-11 13:37:30 +0545

    second commit

---
+++ src/main/java/com/example/Spring/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.Spring.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String showHomePage(){
+        return "Welcome to the java world";
+    }
+
+
+}
